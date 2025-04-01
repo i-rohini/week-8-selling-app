fdb79d3702f640afbfa234db575415dfde2f9953
Rohini Garg <rohini2garg1@gmail.com>
2025-04-01 22:08:13 +0530

    auth endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,74 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db");
+const bcrypt = require("bcrypt");
+const { z } = require("zod");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = "12dfs345f675g";
 
-adminRouter.post("/signup", function(req, res) {
+adminRouter.post("/signup", async function(req, res) {
+  const requiredBody = z.object({
+    email: z.string().min(5).max(100).email(),
+    password: z.string().min(5).max(),
+    firstName: z.string().min().max(30),
+    lastName: z.string().min().max(30)
+  })
+
+  const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+  if (!parsedDataWithSuccess.success) {
+    res.json({
+      message: "Incorrect format",
+      error: parsedDataWithSuccess.error
+    });
+    return
+  }
+
+  const { email, password, firstName, lastName } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  await adminModel.create({
+    email: email,
+    password: hashedPassword,
+    firstName: firstName,
+    lastName: lastName,
+  })
   res.json({
-    message: "signup endpoint"
-  });
+    message: "signup succeeded"
+  })
 });
 
-adminRouter.post("/signin", function(req, res) {
-  res.json({
-    message: "signup endpoint"
+
+adminRouter.post("/signin", async function(req, res) {
+  const { email, password } = req.body;
+
+  const admin = await adminModel.findOne({
+    email: email
   });
+
+  if (!admin) {
+    res.status(403).json({
+      message: "admin does not exit in our db"
+    });
+    return
+  };
+
+  const passwordMatch = await bcrypt.compare(password, admin.password);
+  console.log(passwordMatch);
+
+  if (passwordMatch) {
+    const token = jwt.sign({
+      id: admin._id.toString()
+    }, JWT_ADMIN_PASSWORD)
+    res.json({
+      token: token
+    })
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials"
+    })
+  }
 });
 
 adminRouter.post("/course", function(req, res) {
