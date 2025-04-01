9d25bb56471cc84e71f789229df87dc3f85d6d5e
Rohini Garg <rohini2garg1@gmail.com>
2025-04-01 12:13:37 +0530

    Only start if database is up

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { userRouter } = require("./routes/user")
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+const app = express();
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
+
+async function main() {
+  await mongoose.connect("mongodb+srv://rohini2garg1:dWtTJHBy2oMR2Mhd@cluster0.w8pwsdp.mongodb.net/coursera-app");
+app.listen(3000);
+console.log("listen on port 3000");
+}
+
+main()
